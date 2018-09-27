2838f2a597df652496e968d2fbb2f7e1d248edeb
GanYihuan <583520052@qq.com>
2018-09-27 23:50:43 +0800

    7-11-12-13-14-15-16: 数据

--- pages/movies/movies.js
+++ pages/movies/movies.js
@@ -2,8 +2,10 @@ var util = require('../../utils/util.js')
 var app = getApp()
 
 Page({
-	// RESTFul API JSON
-	// SOAP XML
+  /*
+	RESTFul API JSON
+	SOAP XML
+  */
 	data: {
 		inTheaters: {},
 		comingSoon: {},
@@ -23,6 +25,51 @@ Page({
 		this.getMovieListData(comingSoonUrl, 'comingSoon', '即将上映')
 		this.getMovieListData(top250Url, 'top250', '豆瓣Top250')
 	},
+	getMovieListData: function(url, settedKey, categoryTitle) {
+		var that = this
+		wx.request({
+			url: url,
+      /* OPTIONS, GET, HEAD, POST, PUT, DELETE, TRACE, CONNECT */
+      method: 'GET',
+			header: {
+				'Content-Type': 'json'
+			},
+			success: function(res) {
+        // console.log(res)
+				that.processDoubanData(res.data, settedKey, categoryTitle)
+			},
+			fail: function(error) {
+        /* 断网的时候调用 */
+				console.log(error)
+			}
+		})
+	},
+	processDoubanData: function(moviesDouban, settedKey, categoryTitle) {
+		var movies = []
+		for (var idx in moviesDouban.subjects) {
+			var subject = moviesDouban.subjects[idx]
+			var title = subject.title
+			if (title.length >= 6) {
+				title = title.substring(0, 6) + '...'
+			}
+			var temp = {
+        /* [1,1,1,1,1] [1,1,1,0,0] */
+				stars: util.convertToStarsArray(subject.rating.stars),
+				title: title,
+				average: subject.rating.average,
+				coverageUrl: subject.images.large,
+				movieId: subject.id
+			}
+			movies.push(temp)
+    }
+    /* js 动态赋值 */
+		var readyData = {}
+		readyData[settedKey] = {
+			categoryTitle: categoryTitle,
+			movies: movies
+		}
+		this.setData(readyData)
+	},
 	onMoreTap: function(event) {
 		var category = event.currentTarget.dataset.category
 		wx.navigateTo({
@@ -33,23 +80,6 @@ Page({
 		var movieId = event.currentTarget.dataset.movieid
 		wx.navigateTo({
 			url: 'movie-detail/movie-detail?id=' + movieId
-		})
-	},
-	getMovieListData: function(url, settedKey, categoryTitle) {
-		var that = this
-		wx.request({
-			url: url,
-			method: 'GET', // OPTIONS, GET, HEAD, POST, PUT, DELETE, TRACE, CONNECT
-			header: {
-				'Content-Type': 'json'
-			},
-			success: function(res) {
-				that.processDoubanData(res.data, settedKey, categoryTitle)
-			},
-			fail: function(error) {
-				// fail
-				console.log(error)
-			}
 		})
 	},
 	onCancelImgTap: function(event) {
@@ -69,30 +99,5 @@ Page({
 		var text = event.detail.value
 		var searchUrl = app.globalData.doubanBase + '/v2/movie/search?q=' + text
 		this.getMovieListData(searchUrl, 'searchResult', '')
-	},
-	processDoubanData: function(moviesDouban, settedKey, categoryTitle) {
-		var movies = []
-		for (var idx in moviesDouban.subjects) {
-			var subject = moviesDouban.subjects[idx]
-			var title = subject.title
-			if (title.length >= 6) {
-				title = title.substring(0, 6) + '...'
-			}
-			// [1,1,1,1,1] [1,1,1,0,0]
-			var temp = {
-				stars: util.convertToStarsArray(subject.rating.stars),
-				title: title,
-				average: subject.rating.average,
-				coverageUrl: subject.images.large,
-				movieId: subject.id
-			}
-			movies.push(temp)
-		}
-		var readyData = {}
-		readyData[settedKey] = {
-			categoryTitle: categoryTitle,
-			movies: movies
-		}
-		this.setData(readyData)
 	}
 })
