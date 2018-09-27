ac2b7c25b7fcff7d28fe6739c9c9b8f362f651b2
GanYihuan <583520052@qq.com>
2018-09-27 09:56:34 +0800

    style

--- pages/posts/post.js
+++ pages/posts/post.js
@@ -2,7 +2,7 @@ var postsData = require('../../data/posts-data.js')
 
 Page({
 	data: {
-		//小程序总是会读取data对象来做数据绑定，这个动作我们称为动作A
+		// 小程序总是会读取data对象来做数据绑定，这个动作我们称为动作A
 		// 而这个动作A的执行，是在onLoad函数执行之后发生的
 	},
 	onLoad: function() {
@@ -11,7 +11,6 @@ Page({
 			postList: postsData.postList
 		})
 	},
-
 	onPostTap: function(event) {
 		var postId = event.currentTarget.dataset.postid
 		// console.log("on post id is" + postId);
@@ -19,7 +18,6 @@ Page({
 			url: 'post-detail/post-detail?id=' + postId
 		})
 	},
-
 	onSwiperTap: function(event) {
 		// target 和currentTarget
 		// target指的是当前点击的组件 和currentTarget 指的是事件捕获的组件
